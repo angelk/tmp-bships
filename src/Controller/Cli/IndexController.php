87bd1eb45d2ee6f4f5fd7d1d01c6803db12e0237
po_taka <angel.koilov@gmail.com>
2016-01-17 15:09:21 +0200

    better code for cli controller

--- src/Controller/Cli/IndexController.php
+++ src/Controller/Cli/IndexController.php
@@ -2,6 +2,9 @@
 
 namespace Controller\Cli;
 
+use Model\Battlefield\BattlefieldFactory;
+use Model\Battlefield\Battlefield;
+
 /**
  * Description of IndexController
  *
@@ -11,35 +14,15 @@ class IndexController extends \Controller\AbstractController
 {
     public function homeAction()
     {
-        $battlefieldFactory = new \Model\Battlefield\BattlefieldFactory();
+        $battlefieldFactory = new BattlefieldFactory();
         $battlefield = $battlefieldFactory->createDefaultBattlefield();
-        
         $battlefield->setEventDispacher($this->getEventDispacher());
         
         $newGame = true;
         
         do {
-            while ($battlefield->isThereNonSunkBattleship()) {
-                $visualizer = $this->getVisualizerFactory()->create($battlefield);
-                if (($lastShotStatus = $visualizer->getLastShotStatus())) {
-                    echo $lastShotStatus . PHP_EOL;
-                }
-                echo PHP_EOL . $visualizer->getFieldOutput(). PHP_EOL;
-                $shot = null;
-                $pointFactory = new \Model\Battlefield\Point\PointFactory();
-                while ($shot === null) {
-                    echo "Enter point coordinates: ";
-                    $coordinates = trim(fgets(STDIN));
-                    try {
-                        $shot = $pointFactory->createPoint($coordinates);
-                        $battlefield->shoot($shot);
-                    } catch (\Exception $e) {
-                        echo "Error. ". $e->getMessage() . PHP_EOL;
-                    }
-                }
-            }
-
-            echo "Game completed in " . $battlefield->getShots()->count() . ' shots' . PHP_EOL;
+            $this->playGame($battlefield);
+            echo "Game completed in {$battlefield->getShots()->count()} shots" . PHP_EOL;
             
             $newGame = null;
             while (!in_array($newGame, ['y', 'n'])) {
@@ -48,6 +31,45 @@ class IndexController extends \Controller\AbstractController
             }
             
         } while ($newGame === 'y');
+        
         return 0;
+    }
+        
+    /**
+     * @param Battlefield $battlefield
+     */
+    protected function playGame(Battlefield $battlefield)
+    {
+        while ($battlefield->isThereNonSunkBattleship()) {
+            $visualizer = $this->getVisualizerFactory()->create($battlefield);
+            if (($lastShotStatus = $visualizer->getLastShotStatus())) {
+                echo $lastShotStatus . PHP_EOL;
+            }
+            echo PHP_EOL . $visualizer->getFieldOutput(). PHP_EOL;
+            $this->shoot($battlefield);
+        }
+    }
+    
+    /**
+     * @param Battlefield $battlefield
+     * @return \Model\Battlefield\Point\PointInterface
+     */
+    protected function shoot(Battlefield $battlefield)
+    {
+        $pointFactory = new \Model\Battlefield\Point\PointFactory();
+        $shot = null;
+        while ($shot === null) {
+            echo "Enter point coordinates: ";
+            $coordinates = trim(fgets(STDIN));
+            try {
+                $shot = $pointFactory->createPoint($coordinates);
+                $battlefield->shoot($shot);
+            } catch (\Model\Battlefield\Exception\HumanReadableException $e) {
+                echo "Error. ". $e->getMessage() . PHP_EOL;
+                $shot = null;
+            }
+        }
+        
+        return $shot;
     }
 }
