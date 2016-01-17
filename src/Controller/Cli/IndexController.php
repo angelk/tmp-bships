f7fbf6585877aff7f3af777b65002ba5a7a82b97
po_taka <angel.koilov@gmail.com>
2016-01-17 14:58:35 +0200

    added new game option for cli

--- src/Controller/Cli/IndexController.php
+++ src/Controller/Cli/IndexController.php
@@ -16,27 +16,38 @@ class IndexController extends \Controller\AbstractController
         
         $battlefield->setEventDispacher($this->getEventDispacher());
         
-        while ($battlefield->isThereNonSunkBattleship()) {
-            $visualizer = $this->getVisualizerFactory()->create($battlefield);
-            if (($lastShotStatus = $visualizer->getLastShotStatus())) {
-                echo $lastShotStatus . PHP_EOL;
-            }
-            echo PHP_EOL . $visualizer->getFieldOutput(). PHP_EOL;
-            $shot = null;
-            $pointFactory = new \Model\Battlefield\Point\PointFactory();
-            while ($shot === null) {
-                echo "Enter point coordinates: ";
-                $coordinates = trim(fgets(STDIN));
-                try {
-                    $shot = $pointFactory->createPoint($coordinates);
-                    $battlefield->shoot($shot);
-                } catch (\Exception $e) {
-                    echo "Error. ". $e->getMessage() . PHP_EOL;
+        $newGame = true;
+        
+        do {
+            while ($battlefield->isThereNonSunkBattleship()) {
+                $visualizer = $this->getVisualizerFactory()->create($battlefield);
+                if (($lastShotStatus = $visualizer->getLastShotStatus())) {
+                    echo $lastShotStatus . PHP_EOL;
+                }
+                echo PHP_EOL . $visualizer->getFieldOutput(). PHP_EOL;
+                $shot = null;
+                $pointFactory = new \Model\Battlefield\Point\PointFactory();
+                while ($shot === null) {
+                    echo "Enter point coordinates: ";
+                    $coordinates = trim(fgets(STDIN));
+                    try {
+                        $shot = $pointFactory->createPoint($coordinates);
+                        $battlefield->shoot($shot);
+                    } catch (\Exception $e) {
+                        echo "Error. ". $e->getMessage() . PHP_EOL;
+                    }
                 }
             }
-        }
-        
-        echo "Game completed in " . $battlefield->getShots()->count() . ' shots' . PHP_EOL;
-        return [];
+
+            echo "Game completed in " . $battlefield->getShots()->count() . ' shots' . PHP_EOL;
+            
+            $newGame = null;
+            while (!in_array($newGame, ['y', 'n'])) {
+                echo "New game(y/n): ";
+                $newGame = trim(fgets(STDIN));
+            }
+            
+        } while ($newGame === 'y');
+        return 0;
     }
 }
